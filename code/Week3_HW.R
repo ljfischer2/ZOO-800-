383c43fe42ce5ec1d6b7cae4fa7d24c169b3836d
Jaden Nguyen <jnguyen17@luc.edu>
2025-09-18 15:36:56 -0500

    Notes for Parts 1&2, Finished part of Part 5 (chlorophyll section)

--- code/Week3_HW.R
+++ code/Week3_HW.R
@@ -11,18 +11,33 @@ print(temp_state)
 
 species_counts <- c(Bluegill = 12, Bass = 7, Sunfish = 21, Carp = 3)
 
+#sum to find total number of fish counted
 fish_counted <- sum(species_counts)
 fish_counted
 
+#find species with highest count 
 highest_count <- max(species_counts)
 highest_count_species <- names(which.max(species_counts))
 
+#set chlorophyll concentration values
 chlorophyll_conc <- matrix(c(21, 22, 23, 24, 25, 26, 27, 28, 29), nrow = 3, ncol = 3)
+#name rows: depths
 rownames(chlorophyll_conc) <- c("Surface", "Mid", "Bottom")
+#name columns: days 
 colnames(chlorophyll_conc) <- c("Day 1", "Day 2", "Day 3")
 print(chlorophyll_conc)
 
+#average the values for each row (depth)
 depth_averages <- (rowMeans(chlorophyll_conc))
 depth_averages
 
+#Part 5
+
+#mean chlorophyll concentrations (across 3 days) for each depth
+means_depths <- apply(chlorophyll_conc, 1, mean)
+means_depths
+
+#mean chlorophyll concentrations (across 3 depths) for each day
+means_days <- apply(chlorophyll_conc, 2, mean)
+means_days
                   
